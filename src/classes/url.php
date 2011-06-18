b883d170d51a90615c1c26492c876262e39e7cbb
Brandon R. Stoner <monokrome@limpidtech.com>
2011-06-17 22:45:34 -0700

    Added class for getting URLs.

---
+++ src/classes/url.php
@@ -0,0 +1,22 @@
+<?php
+
+define('SITE_BASE_URL', '');
+
+class URL
+{
+	static function absolute($url)
+	{
+		return '/' . SITE_BASE_URL . $url;
+	}
+
+	static function relative($url, $append_slash = true)
+	{
+		$final_url = $_SERVER['REQUEST_URI'];
+
+		if ($append_slash)
+			$final_url .= '/';
+
+		return $final_url . $url;
+	}
+}
+
