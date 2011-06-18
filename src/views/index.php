15a440a0c5795a8957b1e736d7d997080b510c7d
Brandon R. Stoner <monokrome@limpidtech.com>
2011-06-17 22:31:05 -0700

    Added some description to the index page.

--- src/views/index.php
+++ src/views/index.php
@@ -1,1 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>PHP-Micro</title>
+	</head>
+
+	<body>
+		<h3>Welcome to PHP-Micro.</h3>
+
+		<p>
+			This is a <strong>very</strong> minimal framework
+			written in PHP. It exists for the sole purpose of
+			providing the functionality needed by almost every
+			webapp - and nothing more.
+		</p>
+
+		<p>
+			It makes use of a consistent class organization
+			scheme, alongside any utility classes that the
+			developers feel are necessary for most webapps.
+		</p>
+
+		<p>
+			If you are expecting it to do everything and more -
+			and you absolutely must use PHP - then you should
+			probably look into another framework such as
+			<a href="http://kohanaframework.org">KohanaPHP</a>.
+		</p>
+
+		<p>
+			<em>
+				Note that the developers of PHP-Micro firmly
+				believes large webapps should never be
+				developed in the PHP language. The should
+				be built in something that scales better
+				to a large number requests - which PHP
+				is very bad at. There are many
+				<a href="http://djangoproject.com">smarter</a>,
+				<a href="http://expressjs.com">better</a>,
+				and
+				<a href="http://sinatrarb.com">simpler</a>
+				methods to develop large projects for the web.
+				
+			</em>
+		</p>
+	</body>
+</html>
 
