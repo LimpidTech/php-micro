e66a34075cf3eaa5e6acba970de3271e607cfb83
Brandon R. Stoner <monokrome@limpidtech.com>
2011-06-17 23:50:43 -0700

    Made views work better.

--- src/classes/view.php
+++ src/classes/view.php
@@ -2,30 +2,34 @@
 
 	define('VIEW_BASE_DIR', MICRO_BASE_DIR . '/views');
 	define('VIEW_DEFAULT_NAME', 'index');
-	define('VIEW_DEFAULT_EXTENSION', 'php');
+	define('VIEW_DEFAULT_EXTENSION', '.php');
 
 	class View
 	{
 		private $name;
-		private $extension;
 
-		public static function factory($name=VIEW_DEFAULT_NAME, $extension=VIEW_DEFAULT_EXTENSION)
+		public static function factory($name=VIEW_DEFAULT_NAME)
 		{
-			return new View($name, $extension);
+			return new View($name);
 		}
 
-		public function __construct($name=VIEW_DEFAULT_EXTENSION, $extension=VIEW_DEFAULT_EXTENSION)
+		public function __construct($name=VIEW_DEFAULT_EXTENSION)
 		{
 			$this->name = $name;
-			$this->extension = $extension;
 		}
 
 		public function get_view_path()
 		{
-			return VIEW_BASE_DIR . '/' . $this->name . '.' . $this->extension;
+			$name = $this->name;
+			$extension = strrchr($name, '.');
+
+			if ($extension === false)
+				$name .= VIEW_DEFAULT_EXTENSION;
+
+			return VIEW_BASE_DIR . '/' . $name;
 		}
 
-		public function render($data=Array(), $print = true, $name=VIEW_DEFAULT_NAME, $extension=VIEW_DEFAULT_EXTENSION)
+		public function render($data=Array(), $print = true)
 		{
 			extract($data);
 
@@ -34,7 +38,7 @@
 			require($this->get_view_path());
 			$output = ob_get_contents();
 
-			if ($print)
+			if ($print === true)
 				ob_end_flush();
 			else
 				ob_end_clean();
